a94a4ded96177061a5753211abd57e3625ef273b
Matthew J <tophermose@gmail.com>
2018-11-06 11:42:31 -0500

    Add files via upload

---
+++ css_div_and_span/js/joseph_js1.js
@@ -0,0 +1,43 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function noun_function() {
+    var noun_node_list = document.getElementsByClassName("noun");
+    //This creates a NodeList of all tags with the given class: "noun".
+
+    for (var i = 0; i < noun_node_list.length; i++) {
+        //This creates a loop that will cycle through every instance of the class: "noun" and apply the code within the block to it.
+        var x = document.getElementsByClassName("main")[0];
+        //This creates a variable based on the class name "main" with the position of this class' use being [0] to allow for further manipulation
+        //"Main" is a parent class that allows child classes to be manipulated under one umbrella class name. 
+        x.getElementsByClassName("noun")[i].style.color = "red";
+        //This uses the variable x, and applies an attribute change to every <span> with the class: "noun", iterating through every noun
+    }
+}
+
+function verb_function() {
+    var verb_node_list = document.getElementsByClassName("verb");
+
+    for (var i = 0; i < verb_node_list.length; i++) {
+        var x = document.getElementsByClassName("main")[0];
+        x.getElementsByClassName("verb")[i].style.color = "blue";
+    }
+}
+
+function adverb_function() {
+    var adverb_node_list = document.getElementsByClassName("adverb");
+
+    for (var i = 0; i < adverb_node_list.length; i++) {
+        var x = document.getElementsByClassName("main")[0];
+        x.getElementsByClassName("adverb")[i].style.backgroundColor = "yellow";
+        //x.getElementsByClassName("adverb")[i].style.color = "black";
+    }
+}
+window.onload = noun_function();
+window.onload = verb_function();
+window.onload = adverb_function();
+
+
+
---
+++ css_div_and_span/js/js_alert.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function alert_function() {
+    alert("this works");
+}
+
+function prompt_function() {
+    window.prompt("What is your name?");
+}
+window.onboad = alert_function(); prompt_function();
+
+    
